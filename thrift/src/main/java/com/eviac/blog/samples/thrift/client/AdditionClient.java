ef27aca9fbb4a515b27e97e7ad6c795797bfaf37
skmbw <tongku2008@126.com>
2016-10-08 16:25:02 +0800

    初始化thrift学习项目

---
+++ thrift/src/main/java/com/eviac/blog/samples/thrift/client/AdditionClient.java
@@ -0,0 +1,35 @@
+package com.eviac.blog.samples.thrift.client;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+import com.eviac.blog.samples.thrift.server.AdditionService;
+
+public class AdditionClient {
+
+	public static void main(String[] args) {
+
+		try {
+			TTransport transport;
+
+			transport = new TSocket("localhost", 9090);
+			transport.open();
+
+			TProtocol protocol = new TBinaryProtocol(transport);
+			AdditionService.Client client = new AdditionService.Client(protocol);
+			long d = System.currentTimeMillis();
+			System.out.println(client.add(100, 210));
+			System.out.println(System.currentTimeMillis() - d);
+			transport.close();
+		} catch (TTransportException e) {
+			e.printStackTrace();
+		} catch (TException x) {
+			x.printStackTrace();
+		}
+	}
+
+}
